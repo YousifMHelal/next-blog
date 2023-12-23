042cf9fa9e4c4bc71176ab7bfbf4d4ca78fdb65b
YousifMHelal <yousifM.Helal@gmail.com>
2023-12-23 20:44:27 +0200

    new

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
+import bubble from "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import {
